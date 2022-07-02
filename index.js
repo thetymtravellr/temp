8ba67eea2d13c5b0a493cf62f3cee9b044364b38
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-07-02 13:52:09 +0600

    latest

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send('node server running')
+})
+
+app.listen(port, () => {
+    console.log('listening');
+})
